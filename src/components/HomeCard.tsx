036f32e4be1d69fdea8901b10c9f3ee7454051ce
maximestajszczyk <maxime.stajszczyk@ecole-isitech.fr>
2024-08-30 12:53:18 +0200

    feat: add hover in card

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -39,13 +39,19 @@ export const HomeCard: FC/*<Props>*/ = (/*props*/) => {
 
     return (
         <>
-            <div className="flex flex-col border-4 border-orange-500 shadow-xl">
-                <img src={picture}></img>
-                <div className="bg-stone-600 grid grid-rows-2 grid-flow-col gap-4 border-t-4 border-orange-500">
+            <div className="relative flex flex-col border-4 border-orange-500 shadow-xl group">
+                <img src={picture} alt="Titanic" className="w-full h-auto" />
+                <div className="bg-stone-600 grid grid-rows-2 grid-flow-col gap-4 border-t-4 border-orange-500 p-2">
                     <p className="mt-2 ml-2 text-orange-500 underline">{title}</p>
                     <p className="mb-2 ml-2 text-white">{note}</p>
                     <p className="mt-2 mr-2 text-right text-white">{author}</p>
                     <p className="mb-2 mr-2 text-right text-white">{date}</p>
+                </div>
+
+                {/* Overlay div */}
+                <div className="absolute inset-0 bg-gray-900 bg-opacity-90 text-white p-4 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                    <p className="text-xl underline text-orange-500 mb-2">{title}</p>
+                    <p className="text-md text-white">Summary</p>
                 </div>
             </div>
         </>
