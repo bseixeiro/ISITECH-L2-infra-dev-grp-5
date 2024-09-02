e18a2b3e8d411d59b8a18f13369954f057b28cb6
bseixeiro <benjamin.seixeiro@ecole-isitech.fr>
2024-09-02 20:20:29 +0200

    init Connexion Page

--- src/components/ConnexionPage.tsx
+++ src/components/ConnexionPage.tsx
@@ -7,14 +7,16 @@ type IProps = {
 
 const ConnexionPage: FC<IProps> = () => {
   return (
-    <div className="">
-      <input type="text" placeholder="email"></input>
-      <input type="text" placeholder="mdp"></input>
-      <Button
-        label="Connexion"
-        onClick={() => console.log("")}
-        className="bg-orange-500 text-black font-semibold py-2 px-4 rounded hover:bg-orange-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-orange-300 border border-stone-800 hover:border-gray-500"
-      ></Button>
+    <div className="flex justify-center items-center h-screen">
+      <div>
+        <input type="text" placeholder="email"></input>
+        <input type="text" placeholder="mdp"></input>
+        <Button
+          label="Connexion"
+          onClick={() => console.log("")}
+          className="bg-orange-500 text-black font-semibold py-2 px-4 rounded hover:bg-orange-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-orange-300 border border-stone-800 hover:border-gray-500"
+        ></Button>
+      </div>
     </div>
   );
 };
